5ece34d4d566640a4b77c57c01c9921a247e9df6
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-11-12 08:11:57 +0100

    Split powershell process handlign and cram logic

--- craw.py
+++ craw.py
@@ -7,62 +7,13 @@ from hashlib import md5
 
 
 class Powershell:
-    def __init__(self, workdir: str, env: dict[str, str], variables: dict[str, str]):
+    def __init__(self, workdir: str, env: dict[str, str]):
         """
         Parameters:
             env(dict[str,str]): environment variables
             variables(dict[str,str]): variables set as prelude to the powershell. Will be accessible as `$var`, compared to `$env:var` for env variables
         """
         self.init_ipc_(workdir, env)
-
-        # discard shell prelude
-        mark = self.mark_("")
-        self.receive_until_mark_(mark)
-
-        # Set variables
-        for k, v in variables.items():
-            self.send_line_(f'${k}="{v}"')
-
-        mark = self.mark_("")
-        self.receive_until_mark_(mark)
-
-    def send(self, cmd: str) -> list[str]:
-        self.send_line_(cmd)
-        self.receive_skip_()
-        mark = self.mark_(cmd)
-        return self.receive_until_mark_(mark)
-
-    def __call__(self, cmd: str) -> list[str]:
-        return self.send(cmd)
-
-    def exit(self) -> None:
-        self.send_line_("exit")
-        self.child.wait()
-
-    # Private stuff
-
-    watermark_count_ = randint(0, 2**31)
-
-    def mark_(self, cmd: str) -> str:
-        mark = self.watermark_(cmd)
-        self.send_line_(f'echo "{mark}"')
-        return mark
-
-    def receive_until_mark_(self, mark: str) -> list[str]:
-        result: list[str] = []
-        line = self.receive_line_().strip("\r\n")
-        while line != mark:
-            # do not include our marking machinery in user visible output
-            if mark not in line:
-                result.append(line)
-            line = self.receive_line_().strip("\r\n")
-        return result
-
-    def watermark_(self, cmd: str) -> str:
-        self.watermark_count_ += 1
-        h = md5(str(self.watermark_count_).encode())
-        h.update(cmd.encode())
-        return f"<CRAM> {h.hexdigest()}"
 
     def send_line_(self, line: str) -> None:
         print(line, file=self.outfile)
@@ -71,8 +22,9 @@ class Powershell:
     def receive_line_(self) -> str:
         return self.infile.readline()
 
-    def receive_skip_(self) -> None:
-        _ = self.receive_line_()
+    def exit(self) -> None:
+        self.send_line_("exit")
+        self.child.wait()
 
     def init_ipc_(self, workdir: str, env: dict[str, str]):
         # Source - https://stackoverflow.com/a/54066021
@@ -91,6 +43,58 @@ class Powershell:
         )
         self.outfile = os.fdopen(parent_write, "w", buffering=1)
         self.infile = os.fdopen(parent_read)
+
+
+class Cram:
+    def __init__(self, shell: Powershell, variables: dict[str, str]):
+        self.shell = shell
+        # discard shell prelude
+        mark = self.mark_("")
+        self.receive_until_mark_(mark)
+
+        # Set variables
+        for k, v in variables.items():
+            self.shell.send_line_(f'${k}="{v}"')
+
+        mark = self.mark_("")
+        self.receive_until_mark_(mark)
+
+    def send(self, cmd: str) -> list[str]:
+        self.shell.send_line_(cmd)
+        self.receive_skip_()
+        mark = self.mark_(cmd)
+        return self.receive_until_mark_(mark)
+
+    def __call__(self, cmd: str) -> list[str]:
+        return self.send(cmd)
+
+    # Private stuff
+
+    watermark_count_ = randint(0, 2**31)
+
+    def mark_(self, cmd: str) -> str:
+        mark = self.watermark_(cmd)
+        self.shell.send_line_(f'echo "{mark}"')
+        return mark
+
+    def receive_until_mark_(self, mark: str) -> list[str]:
+        result: list[str] = []
+        line = self.shell.receive_line_().strip("\r\n")
+        while line != mark:
+            # do not include our marking machinery in user visible output
+            if mark not in line:
+                result.append(line)
+            line = self.shell.receive_line_().strip("\r\n")
+        return result
+
+    def watermark_(self, cmd: str) -> str:
+        self.watermark_count_ += 1
+        h = md5(str(self.watermark_count_).encode())
+        h.update(cmd.encode())
+        return f"<CRAM> {h.hexdigest()}"
+
+    def receive_skip_(self) -> None:
+        _ = self.shell.receive_line_()
 
 
 def test(test_lines: list[str], exec: Callable[[str], list[str]]) -> list[str]:
@@ -157,11 +161,12 @@ def main(options: Options, test_file: str) -> None:
     env: dict[str, str] = {k: v for k, v in os.environ.items()}
     cram_special_variables: dict[str, str] = {"TESTDIR": str(os.path.abspath("."))}
     env.update(cram_special_variables)
-    shell = Powershell(workdir=temp_dir, env=env, variables=cram_special_variables)
+    shell = Powershell(workdir=temp_dir, env=env)
+    cram = Cram(shell, variables=cram_special_variables)
 
     with open(test_file, "r", encoding="utf-8") as fin:
         lines = fin.read().replace("\r\n", "\n").split("\n")
-        output = test(lines, shell)
+        output = test(lines, cram)
 
     output_file = (
         test_file if options.promote() else f"{test_file.removesuffix(".t")}.err"

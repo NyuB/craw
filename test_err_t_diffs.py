f272c951bc939f3f8e4121d23c1bfc7515f145c5
Brice Decaestecker <brice.decaestecker@gmx.fr>
2026-08-14 23:09:42 +0200

    Remove deprecated usage of os.system

--- test_err_t_diffs.py
+++ test_err_t_diffs.py
@@ -2,7 +2,7 @@
 Test helper to run after a cram test, checking that .err file have no difference with .t files
 """
 
-import os
+import subprocess
 import sys
 
 
@@ -10,10 +10,20 @@ def main(t_files: list[str]):
     error = False
     for t_file in t_files:
         err_file = t_file.replace(".t", ".err")
-        err_code = os.system(
-            f"git --no-pager diff -p --no-index -- {t_file} {err_file}"
+        print("Checking", t_file, err_file)
+        err_code = subprocess.run(
+            args=[
+                "git",
+                "--no-pager",
+                "diff",
+                "-p",
+                "--no-index",
+                "--",
+                t_file,
+                err_file,
+            ],
         )
-        if err_code != 0:
+        if err_code.returncode != 0:
             error = True
     if error:
         sys.exit(1)
